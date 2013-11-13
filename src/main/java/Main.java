2c42f1694d9ecd2f88b8e6b2982d87a272d6e254
Guilherme dos Santos Fujiyoshi <gfuji1211@gmail.com>
2013-11-13 15:38:46 -0200

    Correções

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,7 @@ package main.java;
 
 public class Main {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
     }
 
