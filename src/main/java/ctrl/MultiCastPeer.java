1d27cf27dd0c3d5a60b6f4bdbb91efa3cf515f82
fuji <gfuji1211@gmail.com>
2013-11-18 16:06:02 -0200

    Continuação MultiCast

--- src/main/java/ctrl/MultiCastPeer.java
+++ src/main/java/ctrl/MultiCastPeer.java
@@ -2,21 +2,31 @@ package ctrl;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
 
-public class MultiCastPeer implements Runnable, Serializable {
+public class MultiCastPeer extends Thread implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private final String HOST = "229.0.0.50";
     private final int PORT = 5050;
+    private final int TIMEOUT = 20000;
+    private static int USER = 0;
+    private String usuario;
     private MulticastSocket socket;
+    private InetAddress group;
 
     public MultiCastPeer() {
         try {
-            InetAddress group = InetAddress.getByName(HOST);
+            group = InetAddress.getByName(HOST);
             socket = new MulticastSocket(PORT);
+            socket.setSoTimeout(TIMEOUT);
+            socket.joinGroup(group);
+            USER++;
+            usuario = "Usuario " + USER;
+            this.start();
         } catch (UnknownHostException e) {
             System.out.println("Erro no host: " + e.getLocalizedMessage());
         } catch (IOException e) {
@@ -26,8 +36,41 @@ public class MultiCastPeer implements Runnable, Serializable {
 
     @Override
     public void run() {
+        byte[] buffer = new byte[1024];
+        DatagramPacket msgIn = new DatagramPacket(buffer, buffer.length, group, PORT);
+        enviarMensagem("GO");
         while (!socket.isClosed()) {
+            try {
+                socket.receive(msgIn);
+                String res = new String(msgIn.getData()).trim();
+                System.out.println(res);
+                enviarMensagem("Recebido por " + usuario);
+            } catch (IOException e) {
+                System.out.println("Erro I/O: " + e.getLocalizedMessage());
+            } finally {
+                cleanBuffer(buffer);
+            }
+        }
+    }
 
+    /**
+     * Envia uma mensagem para o grupo multicast
+     * 
+     * @param String
+     *            msg
+     */
+    public void enviarMensagem(String msg) {
+        DatagramPacket msgOut = new DatagramPacket(msg.getBytes(), msg.getBytes().length, group, PORT);
+    }
+
+    /**
+     * Método responsável por limpar o buffer de dados
+     * 
+     * @param byte [] buffer
+     */
+    private void cleanBuffer(byte[] buffer) {
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = 0;
         }
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import ctrl.MultiCastPeer;
 import view.MainFrame;
 
 public class Main {
 
     public static void main(String[] args) {
         MainFrame mf = new MainFrame();
+        MultiCastPeer p1 = new MultiCastPeer();
+        MultiCastPeer p2 = new MultiCastPeer();
     }
 }
