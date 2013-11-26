d79aa47df2ee4e296c902fe13558407f83195608
a1047817 <a1047817@einherjar>
2013-11-26 14:34:16 -0200

    Implementação Client

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,13 +1,37 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author a1047817
- */
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Client {
-    
+
+    private int port = 6789;
+    private String ipAddress;
+    private DatagramSocket socket;
+    private InetAddress host;
+
+    public Client(String ipAddress) {
+        try {
+            this.ipAddress = ipAddress;
+            socket = new DatagramSocket();
+            host = InetAddress.getByName(ipAddress);
+        } catch (SocketException ex) {
+            System.out.println("Erro Socket: " + ex.getLocalizedMessage());
+        } catch (UnknownHostException ex) {
+            System.out.println("Erro Host: " + ex.getLocalizedMessage());
+        }
+    }
+
+    public void enviarChute(String msg) {
+        try {
+            DatagramPacket request = new DatagramPacket(msg.getBytes(), msg.getBytes().length, host, port);
+            socket.send(request);
+        } catch (IOException ex) {
+            System.out.println("Erro Envio: " + ex.getLocalizedMessage());
+        }
+    }
 }
