30490ff035e3bc03259378887a31f50edce0da74
Guilherme dos Santos Fujiyoshi <gfuji1211@gmail.com>
2013-11-29 23:31:10 -0200

    Classe criptografia Ok

--- src/main/java/util/Criptografia.java
+++ src/main/java/util/Criptografia.java
@@ -1,7 +1,10 @@
 package util;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
 
@@ -10,8 +13,27 @@ public class Criptografia {
     private final static String ALGORITMO = "RSA";
 
     /**
+     * Retorna um KeyPair. KeyPair.getPrivate() recupera a chave privada e
+     * KeyPair.getPublic() recupera a chave pública
+     * 
+     * @return KeyPair
+     */
+    public static KeyPair geraKeyPair() {
+        KeyPair pair = null;
+        try {
+            final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITMO);
+            final SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            keyGen.initialize(1024, random);
+            pair = keyGen.generateKeyPair();
+        } catch (Exception e) {
+            System.out.println("Erro geração de key pair: " + e.getLocalizedMessage());
+        }
+        return pair;
+    }
+
+    /**
      * Encripta array de bytes utilizando chave privada
-     *
+     * 
      * @param mensagem
      * @param privateKey
      * @return byte[] contendo mensagem encriptada
@@ -30,7 +52,7 @@ public class Criptografia {
 
     /**
      * Encripta array de bytes utilizando chave publica
-     *
+     * 
      * @param mensagem
      * @param publicKey
      * @return byte[] contendo mensagem encriptada
@@ -47,6 +69,13 @@ public class Criptografia {
         return cipherData;
     }
 
+    /**
+     * Decripta um arrey de bytes utilizando chave privada
+     * 
+     * @param msgEncriptada
+     * @param privateKey
+     * @return byte[] contendo mensagem decriptada
+     */
     public static byte[] decriptarComChavePrivada(byte[] msgEncriptada, PrivateKey privateKey) {
         byte[] cipherData = null;
         try {
@@ -59,6 +88,13 @@ public class Criptografia {
         return cipherData;
     }
 
+    /**
+     * Decripta um arrey de bytes utilizando chave pública
+     * 
+     * @param msgEncriptada
+     * @param publicKey
+     * @return byte[] contendo mensagem decriptada
+     */
     public static byte[] decriptarComChavePublica(byte[] msgEncriptada, PublicKey publicKey) {
         byte[] cipherData = null;
         try {
