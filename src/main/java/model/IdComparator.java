e5f13229915e762cb334007ac03e540d0b034de1
marcos.silva <marcos.silva@LASCA25.lasca.local>
2013-12-05 10:35:17 -0200

    faltou uma classe

---
+++ src/main/java/model/IdComparator.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Comparator;
+
+public class IdComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o1.compareTo(o2);
+	}
+		
+
+}
